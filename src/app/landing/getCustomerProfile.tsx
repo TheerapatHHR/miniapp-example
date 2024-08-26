6c72d8f50036bda8574d50dbafba6a4374f410c1
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 16:49:19 +0700

    try fix header

--- src/app/landing/getCustomerProfile.tsx
+++ src/app/landing/getCustomerProfile.tsx
@@ -13,11 +13,12 @@ export const GetCustomerProfile = async () => {
     //     console.error(error);
     // }
     const token = localStorage.getItem("access_token");
-    const url = `api/profile`;
+    // const url = `api/profile`;
+    const url = `https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`;
     const headers = { 'Authorization': `Bearer ${token}`, }
 
     try {
-        return await axios.post(url, { headers });
+        return await axios.post(url, {} ,{ headers });
     } catch (error) {
         return error;
     }
