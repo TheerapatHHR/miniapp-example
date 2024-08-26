df644ffbd185532d1f2d4e147d41bfe2b6a98f3e
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 15:52:15 +0700

    use next request

---
+++ src/app/api/profile/route.tsx
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+import axios from "axios";
+
+
+
+export async function POST(request: NextRequest) {
+    const header = await request.headers.get("Authorization");
+    // const url = `${process.env.PAOTANG_API_URL}/v1/paotangpass/get-customer-profile-sandbox`;
+    const url = `https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangpass/get-customer-profile-sandbox`;
+    try {
+        const resp = await  axios.post(url, {}, { headers: { Authorization: header } });
+        return NextResponse.json({
+            ...resp.data
+        });
+
+    } catch (error: any) {
+        return NextResponse.json({
+            error: error.response.data,
+            status: error.response.status,
+        });
+    }
+}
--- src/app/landing/getCustomerProfile.tsx
+++ src/app/landing/getCustomerProfile.tsx
@@ -1,15 +1,24 @@
 import axios from "axios";
 
 export const GetCustomerProfile = async () => {
+    // const token = localStorage.getItem("access_token");
+    // try{
+    //     return await axios.post(`https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`, {
+    //         headers: {
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //     })
+    // }
+    // catch(error){
+    //     console.error(error);
+    // }
     const token = localStorage.getItem("access_token");
-    try{
-        return await axios.post(`https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-    }
-    catch(error){
-        console.error(error);
+    const url = `api/paotang/profile`;
+    const headers = { 'Authorization': `Bearer ${token}`, }
+
+    try {
+        return await axios.post(url, { headers });
+    } catch (error) {
+        return error;
     }
 }
