3b35d76495951c0b155672b864994b51a9e40e33
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 17:41:12 +0700

    fix request body

--- src/app/landing/initialTransaction.tsx
+++ src/app/landing/initialTransaction.tsx
@@ -6,24 +6,25 @@ export const InitialTransaction = async () => {
     const headers = { 'Authorization': `Bearer ${token}`, }
 
     const paymentInfo = {
-    'compCode': '86279',
-    'paymentMethod': 'KTB-PT',
-    'partnerTxnRef': '{{$uuid}}',
-    'amount': '100.10',
-    'ref1value': '1',
+        'compCode': '86279',
+        'paymentMethod': 'KTB-PT',
+        'partnerTxnRef': '{{$uuid}}',
+        'amount': '100.10',
+        'ref1value': '1',
     }
 
-    const partnerInfo= {
+    const partnerInfo = {
         "deeplink": "https://partner.url.com/paymentResult?ref=abc&destination=miniapp&miniAppUUID={miniAppUUID}"
     }
-    
 
-    const data = new URLSearchParams();
-                data.append('partnerTxnCreatedDt', '2024-04-09T06:24:05+07:00');
-                data.append('paymentInfo', JSON.stringify(paymentInfo));
-                data.append('partnerInfo', JSON.stringify(partnerInfo));
+
+    const data = {
+        'partnerTxnCreatedDt': '2024-04-09T06:24:05+07:00',
+        'paymentInfo': paymentInfo,
+        'partnerInfo': partnerInfo,
+    }
     try {
-        return await axios.post(url, data ,{ headers });
+        return await axios.post(url, data, { headers });
     } catch (error) {
         return error;
     }
