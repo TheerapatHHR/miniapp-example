62ed65dec184ee37bdf1566c66e3c3beec72b74b
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 16:19:44 +0700

    fix path

--- src/app/api/profile/route.tsx
+++ src/app/api/profile/route.tsx
@@ -6,7 +6,7 @@ import axios from "axios";
 export async function POST(request: NextRequest) {
     const header = await request.headers.get("Authorization");
     // const url = `${process.env.PAOTANG_API_URL}/v1/paotangpass/get-customer-profile-sandbox`;
-    const url = `https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`;
+    const url = `https://paotang-pass-sandbox-external-uat.th-service.co.in/v1/paotangpass/get-customer-profile-sandbox`;
     try {
         const resp = await  axios.post(url, {}, { headers: { Authorization: header } });
         return NextResponse.json({
