836f3cfb6621b995aebedac3e01a4128e43072cb
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 15:32:29 +0700

    test get customer profile

--- src/app/landing/getCustomerProfile.tsx
+++ src/app/landing/getCustomerProfile.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const GetCustomerProfile = async ({token}:{token: string}) => {
+    console.log(token);
     try{
         return await axios.post(`https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`, {
             headers: {
