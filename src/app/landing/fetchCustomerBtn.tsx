0080972e57749e7e1f956751ea47fbd74748ceed
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-26 11:08:38 +0700

    extract to ssr

---
+++ src/app/landing/fetchCustomerBtn.tsx
@@ -0,0 +1,7 @@
+"use client"
+import { FetchCustomer } from "./fetchCustomer";
+
+export default function FetchCustomerBtn() {
+    
+  return <button onClick={FetchCustomer}>Click Me</button>;
+}
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -3,6 +3,7 @@ import initAuth from "@/jsBridge/initAuth";
 // import { useState } from "react";
 // import { useRouter } from "next/navigation";
 import axios from 'axios';
+import FetchCustomerBtn from "./fetchCustomerBtn";
 
 type status = {
     status: string;
@@ -115,100 +116,100 @@ export default function Landing() {
     //     )
     // }, [])
 
-    const fetchCustomer = () => {
-        initAuth(
+    // const fetchCustomer = () => {
+    //     initAuth(
 
-            (authorizationCode: string) => {
-                /*
-                  Logic to handle the authorization code received from the native app
-                  after successful authentication
-                */
+    //         (authorizationCode: string) => {
+    //             /*
+    //               Logic to handle the authorization code received from the native app
+    //               after successful authentication
+    //             */
 
-                //example
-                // setStatus({
-                //     status: "init auth success 🎉",
-                //     isLoaded: false,
-                // });
-                console.log("[initAuth] success 🎉");
-                console.log("[initAuth] authCode", authorizationCode);
-                // router.replace(`/?authCode=${authorizationCode}`);
+    //             //example
+    //             // setStatus({
+    //             //     status: "init auth success 🎉",
+    //             //     isLoaded: false,
+    //             // });
+    //             console.log("[initAuth] success 🎉");
+    //             console.log("[initAuth] authCode", authorizationCode);
+    //             // router.replace(`/?authCode=${authorizationCode}`);
 
-                const data = new URLSearchParams();
-                data.append('code', authorizationCode);
-                data.append('grant_type', 'authorization_code');
-                data.append('redirect_uri', "https://miniapp-example-puce.vercel.app/");
+    //             const data = new URLSearchParams();
+    //             data.append('code', authorizationCode);
+    //             data.append('grant_type', 'authorization_code');
+    //             data.append('redirect_uri', "https://miniapp-example-puce.vercel.app/");
 
-                data.append('client_id', "ff0f19c7-7440-4e92-bdbf-8c49b641608d");
-                data.append('client_secret', "KN9RwJEcm1cdnPPo3sPRxHbzbCJR0T");
-                data.append('state', "testtest");
+    //             data.append('client_id', "ff0f19c7-7440-4e92-bdbf-8c49b641608d");
+    //             data.append('client_secret', "KN9RwJEcm1cdnPPo3sPRxHbzbCJR0T");
+    //             data.append('state', "testtest");
 
-                axios.post(`https://paotang-id-external-uat.th-service.co.in/oauth2/token`, data, {
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                })
-                    .then((response) => {
-                        // Handle successful response
-                        console.log(response.data);
-                        accessToken = response.data.access_token;
-                    })
-                    .catch((error) => {
-                        // Handle error
-                        console.error(error);
-                    })
-                    .then(() => {
-                        // axios.post(`https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`, {
-                        //     headers: {
-                        //       Authorization: `Bearer ${accessToken}`,
-                        //     },
-                        //   }).then((response) => {
-                        //     // Handle successful response
-                        //     console.log(response.data);
-                        //   })
-                        //   .catch((error) => {
-                        //     // Handle error
-                        //     console.error(error);
-                        //   });
-                        let config = {
-                            method: 'post',
-                            maxBodyLength: Infinity,
-                            url: 'https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox',
-                            headers: {
-                                // "Access-Control-Allow-Credentials": "true",
-                                // "Access-Control-Allow-Origin": "*",
-                                // "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
-                                // "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                                'Authorization': 'Bearer ' + accessToken,
-                            }
-                        };
+    //             axios.post(`https://paotang-id-external-uat.th-service.co.in/oauth2/token`, data, {
+    //                 headers: {
+    //                     'Content-Type': 'application/x-www-form-urlencoded',
+    //                 },
+    //             })
+    //                 .then((response) => {
+    //                     // Handle successful response
+    //                     console.log(response.data);
+    //                     accessToken = response.data.access_token;
+    //                 })
+    //                 .catch((error) => {
+    //                     // Handle error
+    //                     console.error(error);
+    //                 })
+    //                 .then(() => {
+    //                     // axios.post(`https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox`, {
+    //                     //     headers: {
+    //                     //       Authorization: `Bearer ${accessToken}`,
+    //                     //     },
+    //                     //   }).then((response) => {
+    //                     //     // Handle successful response
+    //                     //     console.log(response.data);
+    //                     //   })
+    //                     //   .catch((error) => {
+    //                     //     // Handle error
+    //                     //     console.error(error);
+    //                     //   });
+    //                     let config = {
+    //                         method: 'post',
+    //                         maxBodyLength: Infinity,
+    //                         url: 'https://paotang-openapi-sandbox-uat.th-service.co.in/v1/paotangid/get-customer-profile-sandbox',
+    //                         headers: {
+    //                             // "Access-Control-Allow-Credentials": "true",
+    //                             // "Access-Control-Allow-Origin": "*",
+    //                             // "Access-Control-Allow-Methods": "GET,DELETE,PATCH,POST,PUT",
+    //                             // "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    //                             'Authorization': 'Bearer ' + accessToken,
+    //                         }
+    //                     };
 
-                        axios.request(config)
-                            .then((response) => {
-                                console.log(JSON.stringify(response.data));
-                            })
-                            .catch((error) => {
-                                console.log(error);
-                            });
-                    })
-            },
-            (errorCode, errorDescription) => {
-                /*
-                  Logic to handle the error received from the native app 
-                  after failed authentication
-                */
+    //                     axios.request(config)
+    //                         .then((response) => {
+    //                             console.log(JSON.stringify(response.data));
+    //                         })
+    //                         .catch((error) => {
+    //                             console.log(error);
+    //                         });
+    //                 })
+    //         },
+    //         (errorCode, errorDescription) => {
+    //             /*
+    //               Logic to handle the error received from the native app 
+    //               after failed authentication
+    //             */
 
-                //example
-                // setStatus({
-                //     status: "init auth failed 😢",
-                //     errorCode: errorCode,
-                //     errorDesc: errorDescription,
-                //     isLoaded: false,
-                // });
-                console.log("[initAuth] failed 😢");
-                console.log("[initAuth] error:", errorCode, errorDescription);
-            }
-        )
-    }
+    //             //example
+    //             // setStatus({
+    //             //     status: "init auth failed 😢",
+    //             //     errorCode: errorCode,
+    //             //     errorDesc: errorDescription,
+    //             //     isLoaded: false,
+    //             // });
+    //             console.log("[initAuth] failed 😢");
+    //             console.log("[initAuth] error:", errorCode, errorDescription);
+    //         }
+    //     )
+    // }
 
     return (
         <div className="bg-cover bg-center h-screen bg-[#141414]">
@@ -234,7 +235,8 @@ export default function Landing() {
                         <div>Woman</div>
                     </div>
                     <div>
-                        <button onClick={fetchCustomer}>Fetch Customer</button>
+                        {/* <button onClick={() => fetchCustomer}>Fetch Customer</button> */}
+                        <FetchCustomerBtn />
                     </div>
                     <div className="grid grid-cols-2 text-white gap-4 p-5">
                         <div className="rounded-md">
