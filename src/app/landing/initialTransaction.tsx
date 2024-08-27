df505b78ccbfd364849d225dfebed54c910b4d9d
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-27 15:40:03 +0700

    axios log

--- src/app/landing/initialTransaction.tsx
+++ src/app/landing/initialTransaction.tsx
@@ -27,7 +27,22 @@ export const InitialTransaction = async () => {
     try {
         return await axios.post(url, data, { headers });
     } catch (error: any) {
-        console.log(error.response);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
         return error;
     }
 }
