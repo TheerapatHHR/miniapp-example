325df72e699b54ee31959b0c2ce17f98989c561a
Theerapat Honghirun <theerapat.h@arise.tech>
2024-08-27 11:55:02 +0700

    add body

--- src/app/api/transaction/route.tsx
+++ src/app/api/transaction/route.tsx
@@ -5,10 +5,11 @@ import axios from "axios";
 
 export async function POST(request: NextRequest) {
     const header = await request.headers.get("Authorization");
+    // const header = await request.headers.get("Authorization");
     // const url = `${process.env.PAOTANG_API_URL}/v1/paotangpass/get-customer-profile-sandbox`;
     const url = `https://oapi-2-legged-external-sandbox-gw-uat.arisetech.dev/pwp/v1/open-api/payment/deeplink`;
     try {
-        const resp = await  axios.post(url, {}, { headers: { Authorization: header } });
+        const resp = await  axios.post(url, request.body, { headers: { Authorization: header } });
         return NextResponse.json({
             ...resp.data
         });
